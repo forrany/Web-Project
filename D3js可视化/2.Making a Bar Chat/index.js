33f471df845583f22fa977af9cec81fbe1ba102f
forrany <gaoyaqi411@126.com>
2019-03-20 16:20:16 +0800

    可视化第二节课程

--- D3js可视化/2.Making a Bar Chat/index.js
+++ D3js可视化/2.Making a Bar Chat/index.js
@@ -1,3 +1,17 @@
+const svg = d3.select('svg');
+const height = +svg.attr('height');
+const width =  +svg.attr('width');
+
+const render = data => {
+    svg.selectAll('rect').data(data)
+        .enter().append('rect')
+            .attr('width',width)
+            .attr('height','200px')
+}
+
 const data = d3.csv('./data.csv').then(data => {
-    console.log(data)
+    data.forEach(item => {
+        item.population = +item.population * 1000
+    })
+    render(data);
 })

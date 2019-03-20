6f48027309d244cec845c94ad530d3fa47cd5f97
forrany <gaoyaqi411@126.com>
2019-03-20 17:09:25 +0800

    更新柱状图的雏形

--- D3js可视化/2.Making a Bar Chat/index.js
+++ D3js可视化/2.Making a Bar Chat/index.js
@@ -3,10 +3,18 @@ const height = +svg.attr('height');
 const width =  +svg.attr('width');
 
 const render = data => {
-    svg.selectAll('rect').data(data)
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.population)])
+        .range([0, width]) //最大值将视图空间充满
+    const yScale = d3.scaleBand()
+        .domain(data.map(d => d.country))
+        .range([0, height])
+
+    svg.selectAll('rect').data(data)  //选择`rect`并绑定数据data，但这个时候没有元素，因此使用enter
         .enter().append('rect')
-            .attr('width',width)
-            .attr('height','200px')
+        .attr('y',d => yScale(d.country))
+        .attr('width', d => xScale(d.population))
+        .attr('height',yScale.bandwidth())
 }
 
 const data = d3.csv('./data.csv').then(data => {

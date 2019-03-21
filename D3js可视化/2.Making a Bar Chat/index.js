5ff3c8be714d60dad94a52e7e18f36933fc65a85
forrany <gaoyaqi411@126.com>
2019-03-21 12:40:27 +0800

    增加坐标轴和margin

--- D3js可视化/2.Making a Bar Chat/index.js
+++ D3js可视化/2.Making a Bar Chat/index.js
@@ -1,20 +1,31 @@
 const svg = d3.select('svg');
 const height = +svg.attr('height');
 const width =  +svg.attr('width');
+const margin = {left:50,top:10,right:20,bottom:30};
+const innerHeight = height - margin.top - margin.bottom;
+const innerWidth = width - margin.left - margin.right;
 
 const render = data => {
+    
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+    
     const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.population)])
-        .range([0, width]) //最大值将视图空间充满
+        .domain([0, d3.max(data,xValue)])
+        .range([0, innerWidth]) //最大值将视图空间充满
     const yScale = d3.scaleBand()
-        .domain(data.map(d => d.country))
-        .range([0, height])
-
-    svg.selectAll('rect').data(data)  //选择`rect`并绑定数据data，但这个时候没有元素，因此使用enter
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+    g.selectAll('rect').data(data)  //选择`rect`并绑定数据data，但这个时候没有元素，因此使用enter
         .enter().append('rect')
-        .attr('y',d => yScale(d.country))
-        .attr('width', d => xScale(d.population))
+        .attr('y',d => yScale(yValue(d)))
+        .attr('width', d => xScale(xValue(d)))
         .attr('height',yScale.bandwidth())
+    svg.append('g').call(d3.axisLeft(yScale));
+    svg.append('g').call(d3.axisBottom(xScale))
+        .attr('transform',`translate(0,${innerHeight})`)
 }
 
 const data = d3.csv('./data.csv').then(data => {
